2e2fadd884a80ac0f61ed0a5ae62f4bbfc886a95
KahoMoon <kahomoon1@gmail.com>
2022-02-23 01:12:47 -0800

    Added search/remove functionality.

--- src/address/data/AddressBook.java
+++ src/address/data/AddressBook.java
@@ -1,18 +1,13 @@
 package address.data;
 import java.util.ArrayList;
-
-/**
- * AddressBook class stores the individual AddressEntry(s) into a collective
- * @author kahom
- * @since 1.00
- */
+import java.util.Locale;
 
 public class AddressBook {
 
     ArrayList<AddressEntry> addressEntryList = new ArrayList<AddressEntry>();
 
     /**
-     * prints out the all information of all elements in the AddressBook
+     * list AddressBook: prints out the all information of all elements in the AddressBook
      */
 
     public void list() {
@@ -26,12 +21,71 @@ public class AddressBook {
 
     /**
      * add AddressBook: add an AddressEntry into AddressBook
-     * @param addressEntry the individual entry which you are adding to the address book
+     * @param addressEntry the entry you want to add to the address book
      */
 
     public void add(AddressEntry addressEntry) {
 
         addressEntryList.add(addressEntry);
+
+    }
+
+    /**
+     * removes a specified entry from the address book
+     * @param addressEntry the entry you want to remove from the address book
+     */
+    public void remove(AddressEntry addressEntry) {
+
+        addressEntryList.remove(addressEntry);
+
+    }
+
+    /**
+     * returns entries that contain the specified search prompt along with index
+     * @param prompt the string you are searching for
+     */
+
+    public void search(String prompt) {
+
+        int index = 0;
+
+        for (AddressEntry i : addressEntryList) {
+
+            if (i.getFirstName().toLowerCase().contains(prompt.toLowerCase()) || i.getLastName().toLowerCase().contains(prompt.toLowerCase()) || i.getStreet().toLowerCase().contains(prompt.toLowerCase()) || i.getCity().toLowerCase().contains(prompt.toLowerCase()) || i.getState().toLowerCase().contains(prompt.toLowerCase()) || i.getPhone().toLowerCase().contains(prompt.toLowerCase()) || i.getEmail().toLowerCase().contains(prompt.toLowerCase())) {
+
+                System.out.println("Index: " + index + "\n");
+                System.out.println(i.toString());
+                System.out.println(("\n"));
+
+            }
+
+            index++;
+
+        }
+
+    }
+
+    /**
+     * returns entries that contain the specified integer (zip) along with the index
+     * @param prompt the integer you are searching for
+     */
+    public void search(int prompt) {
+
+        int index = 0;
+
+        for (AddressEntry i : addressEntryList) {
+
+            if (i.getZip() == prompt) {
+
+                System.out.println("Index: " + index + "\n");
+                System.out.println(i.toString());
+                System.out.println(("\n"));
+
+            }
+
+            index++;
+
+        }
 
     }
 
